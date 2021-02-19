1edbc40ace0b251878819c46c8ad9c5a95b92f30
andrestorres04 <61821537+andrestorres04@users.noreply.github.com>
2021-02-19 15:38:10 +0100

    Tests de integracion

---
+++ Login.IntegrationTests/LoginTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Login.IntegrationTests
+{
+    public class LoginTests : IClassFixture<TestFixture<Startup>>
+    {
+        private HttpClient Client;
+
+        public LoginTests(TestFixture<Startup> fixture)
+        {
+            Client = fixture.Client;
+        }
+
+        [Fact]
+        public async Task LoginTests_LoginCorrect_ReturnsTrue()
+        {
+            // Arrange
+            var request = new
+            {
+                Url = "/login",
+                Body = new
+                {
+                    Username = "jefe",
+                    Password = "f8032d5cae3de20fcec887f395ec9a6a"
+                }
+            };
+
+            // Act
+            var response = await Client.PostAsync(request.Url, ContentHelper.GetStringContent(request.Body));
+            var value = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.True(value == "true");
+        }
+
+        [Fact]
+        public async Task LoginTests_LoginIncorrect_ReturnsFalse()
+        {
+            // Arrange
+            var request = new
+            {
+                Url = "/login",
+                Body = new
+                {
+                    Username = "jefe",
+                    Password = "incorrectpassword"
+                }
+            };
+
+            // Act
+            var response = await Client.PostAsync(request.Url, ContentHelper.GetStringContent(request.Body));
+            var value = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.True(value == "false");
+        }
+
+    }
+}
---
+++ Login.IntegrationTests/VentaTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Login;
+using Login.Core.DTO;
+using Xunit;
+
+namespace Login.IntegrationTests
+{
+    public class VentaTests : IClassFixture<TestFixture<Startup>>
+    {
+        private HttpClient Client;
+
+        public VentaTests(TestFixture<Startup> fixture)
+        {
+            Client = fixture.Client;
+        }
+
+        [Fact]
+        public async Task VentaTest_SizeTotalSales()
+        {
+            // Arrange
+            var request = new
+            {
+                Url = "/Venta",
+            };
+            // Act
+            var response = await Client.GetAsync(request.Url);
+            var value = await response.Content.ReadAsStringAsync();
+            var listaVentas = JsonSerializer.Deserialize<List<VentaDTO>>(value);
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.True(listaVentas.Count == 6);
+        }
+
+    }
+}
